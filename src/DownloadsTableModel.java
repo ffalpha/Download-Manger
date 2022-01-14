1c201daed32b9041f00fe7d23b69d741cb55f896
ffalpha <kalanam217@gmail.com>
2022-01-14 12:11:37 +0530

    Etneds downalod table model

--- src/DownloadsTableModel.java
+++ src/DownloadsTableModel.java
@@ -1,3 +1,27 @@
-public class DownloadsTableModel {
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+
+
+ class DownloadsTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
